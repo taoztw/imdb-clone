fdac59a9ff8325ad669f73bb627615d210412c6b
tz <tztw4723@gmail.com>
2024-05-14 01:31:56 +0800

    create a about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,42 @@
+function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae ea
+        recusandae, ipsa explicabo fuga optio minus sapiente, quasi ducimus
+        rerum rem ullam, eum perferendis consectetur expedita. Provident maxime
+        velit repellendus nihil veritatis pariatur? Quos perspiciatis quaerat
+        qui nulla velit dolore?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit adipisci
+        vitae dignissimos repudiandae laborum rem cum sequi quaerat, fugit
+        voluptatum, odio laboriosam illo consequuntur facere. Sequi tempore non
+        veniam consequuntur?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, ut.
+        Dignissimos molestiae, culpa a eum tempora placeat rerum porro
+        consequuntur voluptatem doloremque cumque impedit, reiciendis ipsum
+        nesciunt sit illum aspernatur maiores. Consequatur exercitationem
+        adipisci porro cupiditate. Obcaecati vel ea, corrupti eius ad inventore
+        quidem nam enim veritatis magni? Nostrum vitae est sit reprehenderit
+        dolor pariatur. Odit maxime blanditiis, at quos quod laborum quam esse
+        est debitis omnis perferendis illum porro quisquam repudiandae impedit,
+        ad similique quas, cumque id optio veritatis eveniet laboriosam
+        sapiente. Tempore nisi fugiat magnam doloribus. Veniam facilis corporis
+        omnis optio consequatur modi labore nihil voluptas, excepturi obcaecati
+        inventore amet, eaque voluptates provident. Ea quisquam explicabo
+        cupiditate quaerat voluptate sint esse vitae cum placeat sunt fuga
+        impedit, praesentium dolores natus, a dicta eum consequatur repudiandae
+        veniam dolor sequi quis ipsum officiis. Repudiandae praesentium
+        voluptatibus necessitatibus esse optio earum, cupiditate quos obcaecati
+        nostrum veniam itaque quo enim labore quisquam.
+      </p>
+    </div>
+  );
+}
+
+export default About;
