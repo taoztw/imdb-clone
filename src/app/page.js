b6bef3c433c767b79f802ad00d3de17284a5c549
tz <tztw4723@gmail.com>
2024-05-15 00:24:37 +0800

    fetch data from tmdb api in the tmdb

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
-function page() {
-  return <div>Home</div>;
+const API_KEY = process.env.API_KEY;
+import Results from "@/components/Results";
+import { DATA } from "@/data/data";
+
+async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const results = DATA.results;
+
+  console.log(results);
+  return <Results results={results} />;
 }
 
-export default page;
+export default Home;
