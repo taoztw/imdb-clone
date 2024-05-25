527b29dc8fecb67055e5d98ba8431b759894de7b
tz <tztw4723@gmail.com>
2024-05-25 18:45:20 +0800

    Suspense

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 function Loading() {
   return (
     <div className="flex justify-center">
-      <Image className="h-52" src="spinner.svg" alt="loading..." />
+      <Image
+        className="h-52"
+        src="spinner.svg"
+        alt="loading..."
+        width={52}
+        height={52}
+      />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
+"use client";
+import Loading from "@/app/loading";
 import NavbarItem from "./NavbarItem";
+import { Suspense } from "react";
 
 function Navbar() {
   return (
     <div className="flex gap-8 justify-center bg-amber-100 dark:bg-gray-600 dark:text-gray-200 p-4 lg:text-lg">
-      <NavbarItem title="Trending" param="fetchTrending" />
-      <NavbarItem title="Top Rated" param="fetchTopRated" />
+      <Suspense fallback={<Loading />}>
+        <NavbarItem title="Trending" param="fetchTrending" />
+        <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </Suspense>
     </div>
   );
 }
